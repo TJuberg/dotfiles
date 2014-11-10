a449524a5892ff665d84dd96be2ab3f1ee96125d
Thomas Juberg <thomas.juberg@gmail.com>
2014-11-10 13:38:46 +0100

    Add tmuxinator command completion Add conky handling to awesome

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -85,6 +85,50 @@ mail       = terminal .. " -e mutt "
 iptraf     = terminal .. " -g 180x54-20+34 -e sudo iptraf-ng -i all "
 musicplr   = terminal .. " -g 130x34-320+16 -e ncmpcpp "
 
+
+function get_conky()
+    local clients = client.get()
+    local conky = nil
+    local i = 1
+    while clients[i]
+    do
+        if clients[i].class == "Conky"
+        then
+            conky = clients[i]
+        end
+        i = i + 1
+    end
+    return conky
+end
+function raise_conky()
+    local conky = get_conky()
+    if conky
+    then
+        conky.ontop = true
+    end
+end
+function lower_conky()
+    local conky = get_conky()
+    if conky
+    then
+        conky.ontop = false
+    end
+end
+function toggle_conky()
+    local conky = get_conky()
+    if conky
+    then
+        if conky.ontop
+        then
+            conky.ontop = false
+        else
+            conky.ontop = true
+        end
+    end
+end
+
+
+
 local layouts = {
     lain.layout.centerwork,
     awful.layout.suit.tile,
@@ -581,6 +625,9 @@ globalkeys = awful.util.table.join(
 
         -- screensaver
         awful.key({ altkey, "Control" }, "l", function () awful.util.spawn("xscreensaver-command -lock") end)
+
+    -- Conky
+    awful.key({}, "Pause", function() toggle_conky() end)
 )
 
 clientkeys = awful.util.table.join(
@@ -680,6 +727,14 @@ awful.rules.rules = {
     { rule = { class = "Gimp", role = "gimp-image-window" },
           properties = { maximized_horizontal = true,
                          maximized_vertical = true } },
+    { rule = { class = "Conky" },
+          properties = {
+                        floating = true,
+                        sticky = true,
+                        ontop = false,
+                        focusable = false,
+                        size_hints = {"program_position", "program_size"}
+    } },
 }
 -- }}}
 
