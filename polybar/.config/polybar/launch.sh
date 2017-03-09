fb99d378d5ce145adc176c7da0fe66067c6664f0
Thomas Juberg <thomas.juberg@gmail.com>
2017-03-09 12:08:57 +0100

    Initial polybar, gnomify i3

---
+++ polybar/.config/polybar/launch.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -x polybar >/dev/null; do sleep 1; done
+
+# Launch bar1 and bar2
+
+#polybar bar1 &
+polybar -m | awk -F ':' '{print "MONITOR="$1" polybar bar1 &"}' | xargs -I '{}' bash -c '{}'
+
+echo "Bars launched..."
