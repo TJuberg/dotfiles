70abeb7e7ec42cbddd6f996b1d7291819920a22a
Thomas Juberg <thomas.juberg@banenor.no>
2020-06-24 03:23:37 +0200

    Minor tweaks for kde+i3

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -10,6 +10,6 @@ while pgrep -x polybar >/dev/null; do sleep 1; done
 
 #polybar bar1 &
 polybar -m | awk -F ':' '{print "MONITOR="$1" polybar bar1 &"}' | xargs -I '{}' bash -c '{}'
-polybar -m | awk -F ':' '{print "MONITOR="$1" polybar bottom &"}' | xargs -I '{}' bash -c '{}'
+#polybar -m | awk -F ':' '{print "MONITOR="$1" polybar bottom &"}' | xargs -I '{}' bash -c '{}'
 
 echo "Bars launched..."
